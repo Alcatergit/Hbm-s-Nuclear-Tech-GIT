e59a23597f8267f0e521d55cdc73f0b4f8e81180
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:05:13 +0800

    Update GeneralConfig.java

--- src/main/java/com/hbm/config/GeneralConfig.java
+++ src/main/java/com/hbm/config/GeneralConfig.java
@@ -47,6 +47,7 @@ public class GeneralConfig {
 	public static boolean callListModels = true;
 	public static boolean useShaders = false;
 	public static boolean useShaders2 = false;
+	public static boolean nukeWarpShockwave = true;
 	public static boolean bloom = true;
 	public static boolean heatDistortion = true;
 	public static boolean enableBabyMode = false;
@@ -78,6 +79,7 @@ public class GeneralConfig {
 	public static int coltanRate = 2;
 	public static int bedrockRate = 50;
     public static boolean enableBlockItemNBTSaving = true;
+	public static boolean evangelion = false;
 	
 	public static void loadFromConfig(Configuration config){
 		final String CATEGORY_GENERAL = "01_general";
@@ -105,6 +107,7 @@ public class GeneralConfig {
 		enableVirus = config.get(CATEGORY_GENERAL, "1.21_enableVirus", false).getBoolean(false);
         enableCrosshairs = config.get(CATEGORY_GENERAL, "1.22_enableCrosshairs", true).getBoolean(true);
         enableBlockItemNBTSaving = config.get(CATEGORY_GENERAL, "1.22_enableBlockItemNBTSaving", true).getBoolean(true);
+		evangelion = config.get(CATEGORY_GENERAL, "1.22_evangelion", false).getBoolean(false);
 		Property shaders = config.get(CATEGORY_GENERAL, "1.23_enableShaders", false);
 		shaders.setComment("Experimental, don't use");
 		useShaders = shaders.getBoolean(false);
@@ -118,6 +121,7 @@ public class GeneralConfig {
 			}
 		useShaders = false;
 		useShaders2 = config.get(CATEGORY_GENERAL, "1.23_enableShaders2", false).getBoolean(false);
+		nukeWarpShockwave = config.get(CATEGORY_GENERAL, "1.23_nukeWarpShockwave", true, "Enables the warp_world shader shockwave that follows nuclear shock fronts.").getBoolean(true);
 		Property ssg_anim = config.get(CATEGORY_GENERAL, "1.24_ssgAnimType", true);
 		ssg_anim.setComment("Which supershotgun reload animation to use. True is Drillgon's animation, false is Bob's animation");
 		ssgAnim = ssg_anim.getBoolean();
