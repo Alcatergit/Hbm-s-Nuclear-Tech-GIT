c60ea62d37cf0f0fc44d4c54976320dbff43d424
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:06:42 +0800

    Update CrucibleTemplateBakedModel.java

--- src/main/java/com/hbm/render/item/CrucibleTemplateBakedModel.java
+++ src/main/java/com/hbm/render/item/CrucibleTemplateBakedModel.java
@@ -6,41 +6,48 @@ import java.util.List;
 import javax.vecmath.Matrix4f;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.lwjgl.input.Keyboard;
+
+import com.hbm.inventory.CrucibleRecipes;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.block.model.ItemOverrideList;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
 public class CrucibleTemplateBakedModel implements IBakedModel {
-	
+
 	TransformType type;
-	
+	ItemStack recipeStack;
+
 	@Override
 	public List<BakedQuad> getQuads(IBlockState state, EnumFacing side, long rand) {
-		// Fixed: No longer returns an empty list in GUI mode, preventing items from appearing transparent.
-		if (CrucibleTemplateRender.INSTANCE.itemModel != null) {
-			return CrucibleTemplateRender.INSTANCE.itemModel.getQuads(state, side, rand);
-		}
-		return Collections.emptyList();
+		IBakedModel im = CrucibleTemplateRender.INSTANCE.itemModel;
+		return type == TransformType.GUI ? Collections.emptyList() : im.getQuads(state, side, rand);
 	}
 
 	@Override
 	public boolean isAmbientOcclusion() {
-		return type != TransformType.GUI && CrucibleTemplateRender.INSTANCE.itemModel.isAmbientOcclusion();
+		IBakedModel im = CrucibleTemplateRender.INSTANCE.itemModel;
+		return type != TransformType.GUI && im.isAmbientOcclusion();
 	}
 
 	@Override
 	public boolean isGui3d() {
-		return type != TransformType.GUI && CrucibleTemplateRender.INSTANCE.itemModel.isGui3d();
+		IBakedModel im = CrucibleTemplateRender.INSTANCE.itemModel;
+		return type != TransformType.GUI && im.isGui3d();
 	}
 
 	@Override
 	public boolean isBuiltInRenderer() {
-		return type == TransformType.GUI;
+		return type == null || type == TransformType.GUI || (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) && type != null);
 	}
 
 	@Override
@@ -50,15 +57,41 @@ public class CrucibleTemplateBakedModel implements IBakedModel {
 
 	@Override
 	public ItemOverrideList getOverrides() {
-		return type == TransformType.GUI ? ItemOverrideList.NONE : CrucibleTemplateRender.INSTANCE.itemModel.getOverrides();
-	}
-	
-	@Override
-	public Pair<? extends IBakedModel, Matrix4f> handlePerspective(TransformType cameraTransformType) {
-		
-		CrucibleTemplateRender.INSTANCE.type = cameraTransformType;
-		this.type = cameraTransformType;
-		return type == TransformType.GUI ? IBakedModel.super.handlePerspective(cameraTransformType) : CrucibleTemplateRender.INSTANCE.itemModel.handlePerspective(cameraTransformType);
+		return new ItemOverrideList(Collections.emptyList()) {
+			@Override
+			public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack, World world, EntityLivingBase entity) {
+				if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+					recipeStack = CrucibleRecipes.getIcon(stack);
+				} else {
+					recipeStack = null;
+				}
+				return originalModel;
+			}
+		};
 	}
 
+	@Override
+	public Pair<? extends IBakedModel, Matrix4f> handlePerspective(TransformType cameraTransformType) {
+
+		IBakedModel im = CrucibleTemplateRender.INSTANCE.itemModel;
+		if (cameraTransformType == TransformType.GUI) {
+			CrucibleTemplateRender.INSTANCE.type = cameraTransformType;
+			this.type = cameraTransformType;
+			return IBakedModel.super.handlePerspective(cameraTransformType);
+		} else {
+			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) && recipeStack != null && !recipeStack.isEmpty()) {
+				CrucibleTemplateRender.INSTANCE.type = cameraTransformType;
+				this.type = cameraTransformType;
+				try {
+					IBakedModel recipeModel = Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(recipeStack, Minecraft.getMinecraft().world, Minecraft.getMinecraft().player);
+					return Pair.of(this, recipeModel.handlePerspective(cameraTransformType).getRight());
+				} catch (Exception ignored) {
+				}
+				return Pair.of(this, null);
+			}
+			CrucibleTemplateRender.INSTANCE.type = null;
+			this.type = null;
+			return im.handlePerspective(cameraTransformType);
+		}
+	}
 }
