91f7556698f4e72178557cbc83372161cb6bf7e1
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:05:09 +0800

    Update ServerProxy.java

--- src/main/java/com/hbm/main/ServerProxy.java
+++ src/main/java/com/hbm/main/ServerProxy.java
@@ -5,7 +5,6 @@ import java.io.File;
 import com.hbm.handler.HbmKeybinds.EnumKeybind;
 import com.hbm.render.amlfrom1710.Vec3;
 import com.hbm.sound.AudioWrapper;
-import com.hbm.sound.V2.AudioWrapperV2;
 
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -38,9 +37,10 @@ public class ServerProxy
 	
 	public void registerMissileItems(IRegistry<ModelResourceLocation, IBakedModel> reg) { }
 
-	public AudioWrapperV2 getLoopedSound(SoundEvent sound, SoundCategory cat, float x, float y, float z, float volume, float range, float pitch, int keepAlive) { return null; }
 	public AudioWrapper getLoopedSound(SoundEvent sound, SoundCategory cat, float x, float y, float z, float volume, float pitch) { return null; }
-	
+
+	public AudioWrapper getLoopedSound(SoundEvent sound, SoundCategory cat, float x, float y, float z, float volume, float range, float pitch, int keepAlive) { return null; }
+
 	public AudioWrapper getLoopedSoundStartStop(World world, SoundEvent sound, SoundEvent start, SoundEvent stop, SoundCategory cat, float x, float y, float z, float volume, float pitch){return null;}
 	
 	public void preInit(FMLPreInitializationEvent evt) {}
