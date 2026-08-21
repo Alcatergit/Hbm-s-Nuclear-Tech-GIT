31da6cf1acec1397c0bdf8f69262cbb35b59ee3c
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:01:29 +0800

    Update NukeFleija.java
    Added the ability to save item NBT data.

--- src/main/java/com/hbm/blocks/bomb/NukeFleija.java
+++ src/main/java/com/hbm/blocks/bomb/NukeFleija.java
@@ -3,6 +3,7 @@ package com.hbm.blocks.bomb;
 import java.util.Random;
 import java.util.List;
 
+import com.hbm.config.GeneralConfig;
 import com.hbm.util.I18nUtil;
 import com.hbm.blocks.ModBlocks;
 import com.hbm.config.BombConfig;
@@ -12,6 +13,7 @@ import com.hbm.interfaces.IBomb;
 import com.hbm.lib.InventoryHelper;
 import com.hbm.main.MainRegistry;
 import com.hbm.tileentity.bomb.TileEntityNukeFleija;
+import com.hbm.items.ModItems;
 
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.block.Block;
@@ -26,6 +28,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
@@ -39,6 +43,11 @@ import net.minecraft.world.World;
 public class NukeFleija extends BlockContainer implements IBomb {
 
 	public static final PropertyInteger FACING = PropertyInteger.create("facing", 2, 5);
+
+	// ========== Added: Field for storing information about players who have been vandalized ==========
+	private EntityPlayer lastBreaker = null;
+	// ========== Added: Mark whether the destruction was caused by an explosion ==========
+	private boolean isExploding = false;
 	
 	public NukeFleija(Material materialIn, String s) {
 		super(materialIn);
@@ -57,22 +66,171 @@ public class NukeFleija extends BlockContainer implements IBomb {
 
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		// ========== Modification: Adding drop control logic upon detonation ==========
+		// If the destruction is caused by an explosion, no items will be dropped.
+		if (isExploding) {
+			return null;
+		}
+
+		// When NBT saving is enabled, the drop is completely controlled by breakBlock.
+		if (GeneralConfig.enableBlockItemNBTSaving) {
+			return null; // Returning null, the fall is controlled by breakBlock.
+		}
+		// ========== Modification complete ==========
+		
 		return Item.getItemFromBlock(ModBlocks.nuke_fleija);
 	}
 
+	// ========== Added: Override the removedByPlayer method to get the destroyed player ==========
+	@Override
+	public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
+		// Recording and destroying player information
+		this.lastBreaker = player;
+
+		// Calling the parent class method to continue executing the disruptive logic
+		boolean result = super.removedByPlayer(state, world, pos, player, willHarvest);
+
+		// Clean up player information
+		this.lastBreaker = null;
+
+		return result;
+	}
+
+	// ========== Modification: Using player information recorded by removedByPlayer ==========
 	@Override
 	public void breakBlock(World world, BlockPos pos, IBlockState state) {
+		// ========== If the destruction is caused by an explosion, the entire block will be cleared, and no items will be dropped. ==========
+		if (isExploding) {
+			TileEntity tileentity = world.getTileEntity(pos);
+			if (tileentity != null) {
+				world.removeTileEntity(pos);
+			}
+			return;
+		}
 
 		TileEntity tileentity = world.getTileEntity(pos);
 
-		if (tileentity instanceof TileEntityNukeFleija) {
-			InventoryHelper.dropInventoryItems(world, pos, tileentity);
+		// ========== New Logic: Configuration File Controls NBT Saving ==========
+		if (!GeneralConfig.enableBlockItemNBTSaving) {
+			// Configure NBT saving is disabled; use the original logic.
+			if (tileentity instanceof TileEntityNukeFleija) {
+				InventoryHelper.dropInventoryItems(world, pos, (TileEntityNukeFleija)tileentity);
+				world.updateComparatorOutputLevel(pos, this);
+			}
+		} else {
+			if (tileentity instanceof TileEntityNukeFleija) {
+				TileEntityNukeFleija nukeFleija = (TileEntityNukeFleija)tileentity;
 
-			world.updateComparatorOutputLevel(pos, this);
+				// Create NBT tags to store block entity data
+				NBTTagCompound tileData = new NBTTagCompound();
+				nukeFleija.writeToNBT(tileData);
+
+				// ========== Check for any items inside ==========
+				boolean hasItems = false;
+				if (tileData.hasKey("inventory") && tileData.getCompoundTag("inventory").hasKey("Items")) {
+					NBTTagList itemsList = tileData.getCompoundTag("inventory").getTagList("Items", 10);
+					hasItems = itemsList.tagCount() > 0;
+				}
+
+				// ========== Player information recorded using removedByPlayer ==========
+				boolean isCreativeMode = (lastBreaker != null && lastBreaker.capabilities.isCreativeMode);
+
+				if (hasItems) {
+					// ========== Internal Items Found: Drops a nuclear bomb containing NBT ==========
+					ItemStack itemstack = new ItemStack(Item.getItemFromBlock(this), 1);
+					NBTTagCompound nbttagcompound = new NBTTagCompound();
+
+					// ========== Simplify NBT data: Only keep BlockEntityTag->inventory->Items ==========
+					NBTTagCompound blockEntityTag = new NBTTagCompound();
+					NBTTagCompound inventoryTag = new NBTTagCompound();
+
+					if (tileData.hasKey("inventory") && tileData.getCompoundTag("inventory").hasKey("Items")) {
+						// Copy only the Items tag and all its child tags.
+						NBTTagList items = tileData.getCompoundTag("inventory").getTagList("Items", 10).copy();
+						inventoryTag.setTag("Items", items);
+					}
+
+					blockEntityTag.setTag("inventory", inventoryTag);
+
+					// Write the BlockEntityTag to the item NBT
+					nbttagcompound.setTag("BlockEntityTag", blockEntityTag);
+					itemstack.setTagCompound(nbttagcompound);
+
+					// Generate drops
+					spawnAsEntity(world, pos, itemstack);
+
+					// Empty the contents
+					nukeFleija.clearSlots();
+				} else if (!isCreativeMode) {
+					// ========== Survival Mode Empty bomb: Drops a regular bomb =========
+					spawnAsEntity(world, pos, new ItemStack(Item.getItemFromBlock(this), 1));
+				}
+				// Creative Mode Empty Bomb: Nothing drops
+			}
 		}
-
+		// Call the parent class's breakBlock but don't let it handle the falling object.
 		super.breakBlock(world, pos, state);
 	}
+	// ========== Modification complete ==========
+
+	// ========== Modification: onBlockPlacedBy method - Support for data recovery from simplified NBT ==========
+	@Override
+	public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+		int i = MathHelper.floor(placer.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+
+		if(i == 0)
+		{
+			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 5), 2);
+		}
+		if(i == 1)
+		{
+			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 3), 2);
+		}
+		if(i == 2)
+		{
+			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 4), 2);
+		}
+		if(i == 3)
+		{
+			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 2), 2);
+		}
+
+		// ========== Edit: Recovering from Simplified NBT Data ==========
+		if (stack.hasTagCompound() && stack.getTagCompound().hasKey("BlockEntityTag")) {
+			TileEntity tileentity = world.getTileEntity(pos);
+			if (tileentity instanceof TileEntityNukeFleija) {
+				NBTTagCompound blockEntityTag = stack.getTagCompound().getCompoundTag("BlockEntityTag");
+
+				if (blockEntityTag.hasKey("inventory")) {
+					NBTTagCompound savedInventory = blockEntityTag.getCompoundTag("inventory");
+
+					// Create complete TileEntity NBT data
+					NBTTagCompound tileData = new NBTTagCompound();
+					tileData.setInteger("x", pos.getX());
+					tileData.setInteger("y", pos.getY());
+					tileData.setInteger("z", pos.getZ());
+
+					// ========== Restore only Items data in inventory ==========
+					NBTTagCompound newInventory = new NBTTagCompound();
+					if (savedInventory.hasKey("Items")) {
+						newInventory.setTag("Items", savedInventory.getTagList("Items", 10).copy());
+					}
+
+					tileData.setTag("inventory", newInventory);
+
+					// Loading data from NBT to block entities
+					((TileEntityNukeFleija) tileentity).readFromNBT(tileData);
+
+					// The marker blocks need to be updated.
+					world.notifyBlockUpdate(pos, state, state, 3);
+
+					// Important: Mark block entities as dirty data to ensure data preservation.
+					tileentity.markDirty();
+				}
+			}
+		}
+	}
+	// ========== Modification complete ==========
 	
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
@@ -100,10 +258,12 @@ public class NukeFleija extends BlockContainer implements IBomb {
         {
         	if(entity.isReady())
         	{
-        		this.onPlayerDestroy(worldIn, pos, state);
-            	entity.clearSlots();
+        		// ========== Modification: Set a detonation flag, then clear the blocks ==========
+        		this.isExploding = true;
+        		entity.clearSlots();
             	worldIn.setBlockToAir(pos);
             	igniteTestBomb(worldIn, pos.getX(), pos.getY(), pos.getZ(), BombConfig.fleijaRadius);
+            	this.isExploding = false;
         	}
         }
 	}
@@ -167,28 +327,6 @@ public class NukeFleija extends BlockContainer implements IBomb {
 	public boolean isFullCube(IBlockState state) {
 		return false;
 	}
-	
-	@Override
-	public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase player, ItemStack stack) {
-		int i = MathHelper.floor(player.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
-		
-		if(i == 0)
-		{
-			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 5), 2);
-		}
-		if(i == 1)
-		{
-			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 3), 2);
-		}
-		if(i == 2)
-		{
-			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 4), 2);
-		}
-		if(i == 3)
-		{
-			world.setBlockState(pos, this.getDefaultState().withProperty(FACING, 2), 2);
-		}
-	}
 
 	@Override
 	public void explode(World world, BlockPos pos) {
@@ -197,10 +335,12 @@ public class NukeFleija extends BlockContainer implements IBomb {
         {
         	if(entity.isReady())
         	{
-        		this.onPlayerDestroy(world, pos, world.getBlockState(pos));
-            	entity.clearSlots();
+        		// ========== Modification: Set a detonation flag, then clear the blocks ==========
+        		this.isExploding = true;
+        		entity.clearSlots();
             	world.setBlockToAir(pos);
             	igniteTestBomb(world, pos.getX(), pos.getY(), pos.getZ(), BombConfig.fleijaRadius);
+            	this.isExploding = false;
         	}
         }
 	}
@@ -223,8 +363,81 @@ public class NukeFleija extends BlockContainer implements IBomb {
 	}
 
 	@Override
-	public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) {
+	public void addInformation(ItemStack stack, World world, List<String> tooltip, ITooltipFlag advanced) {
 		tooltip.add("§b["+ I18nUtil.resolveKey("trait.schrabbomb")+"]§r");
 		tooltip.add(" §e"+I18nUtil.resolveKey("desc.radius", BombConfig.fleijaRadius)+"§r");
+		
+		// ========== Added: Check if the item's NBT data meets the detonation conditions ==========
+		if (isItemReady(stack)) {
+			tooltip.add("§2[Is ready]§r");
+		}
+	}
+	
+	// ========== Added: Helper method, based on the isReady condition of TileEntityNukeFleija ==========
+	private boolean isItemReady(ItemStack stack) {
+		if (stack.hasTagCompound() && stack.getTagCompound().hasKey("BlockEntityTag")) {
+			NBTTagCompound blockEntityTag = stack.getTagCompound().getCompoundTag("BlockEntityTag");
+			
+			if (blockEntityTag.hasKey("inventory")) {
+				NBTTagCompound inventoryTag = blockEntityTag.getCompoundTag("inventory");
+				
+				// Check if it contains the Items tag (the serialization format of ItemStackHandler).
+				if (inventoryTag.hasKey("Items")) {
+					NBTTagList itemsList = inventoryTag.getTagList("Items", 10);
+					
+					// Check if all items required by TileEntityNukeFleija's isReady method are included.
+					boolean hasIgniter1 = false;
+					boolean hasIgniter2 = false;
+					boolean hasPropellant1 = false;
+					boolean hasPropellant2 = false;
+					boolean hasPropellant3 = false;
+					boolean hasCore1 = false;
+					boolean hasCore2 = false;
+					boolean hasCore3 = false;
+					boolean hasCore4 = false;
+					boolean hasCore5 = false;
+					boolean hasCore6 = false;
+					
+					for (int i = 0; i < itemsList.tagCount(); i++) {
+						NBTTagCompound itemTag = itemsList.getCompoundTagAt(i);
+						int slot = itemTag.getByte("Slot");
+						
+						// Check the item ID (use the registered name instead of the string ID).
+						String itemId = itemTag.getString("id");
+						
+						// Check the corresponding item based on the slot (based on the isReady method of TileEntityNukeFleija).
+						if (slot == 0 && itemId.equals(ModItems.fleija_igniter.getRegistryName().toString())) {
+							hasIgniter1 = true;
+						} else if (slot == 1 && itemId.equals(ModItems.fleija_igniter.getRegistryName().toString())) {
+							hasIgniter2 = true;
+						} else if (slot == 2 && itemId.equals(ModItems.fleija_propellant.getRegistryName().toString())) {
+							hasPropellant1 = true;
+						} else if (slot == 3 && itemId.equals(ModItems.fleija_propellant.getRegistryName().toString())) {
+							hasPropellant2 = true;
+						} else if (slot == 4 && itemId.equals(ModItems.fleija_propellant.getRegistryName().toString())) {
+							hasPropellant3 = true;
+						} else if (slot == 5 && itemId.equals(ModItems.fleija_core.getRegistryName().toString())) {
+							hasCore1 = true;
+						} else if (slot == 6 && itemId.equals(ModItems.fleija_core.getRegistryName().toString())) {
+							hasCore2 = true;
+						} else if (slot == 7 && itemId.equals(ModItems.fleija_core.getRegistryName().toString())) {
+							hasCore3 = true;
+						} else if (slot == 8 && itemId.equals(ModItems.fleija_core.getRegistryName().toString())) {
+							hasCore4 = true;
+						} else if (slot == 9 && itemId.equals(ModItems.fleija_core.getRegistryName().toString())) {
+							hasCore5 = true;
+						} else if (slot == 10 && itemId.equals(ModItems.fleija_core.getRegistryName().toString())) {
+							hasCore6 = true;
+						}
+					}
+					
+					// Returns whether the isReady condition of TileEntityNukeFleija is met.
+					return hasIgniter1 && hasIgniter2 && hasPropellant1 && hasPropellant2 && hasPropellant3 && 
+						   hasCore1 && hasCore2 && hasCore3 && hasCore4 && hasCore5 && hasCore6;
+				}
+			}
+		}
+		
+		return false;
 	}
 }
