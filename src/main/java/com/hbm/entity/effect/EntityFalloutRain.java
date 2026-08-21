d0294d2a8855e8c5ac32dc17e1d61448384eebf4
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:04:06 +0800

    Update EntityFalloutRain.java

--- src/main/java/com/hbm/entity/effect/EntityFalloutRain.java
+++ src/main/java/com/hbm/entity/effect/EntityFalloutRain.java
@@ -236,6 +236,7 @@ public class EntityFalloutRain extends EntityChunky implements IConstantRenderer
 		}
 
 		int bottomHeight = lastGapHeight;
+		float totalDamage = 0F;
 
 		for (int y = lastGapHeight; y <= contactHeight; y++) {
 			pos.setY(y);
