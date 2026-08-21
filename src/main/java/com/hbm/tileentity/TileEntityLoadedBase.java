14e98a0fd70f24bfe1b29dd26225d497b0143863
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:03:01 +0800

    Update TileEntityLoadedBase.java
    Further fixes to the popping sound issue in Broadcaster.

--- src/main/java/com/hbm/tileentity/TileEntityLoadedBase.java
+++ src/main/java/com/hbm/tileentity/TileEntityLoadedBase.java
@@ -1,6 +1,7 @@
 package com.hbm.tileentity;
 
 import api.hbm.energy.ILoadedTile;
+import com.hbm.sound.V2.AudioWrapperV2;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityLoadedBase extends TileEntity implements ILoadedTile {
@@ -23,4 +24,13 @@ public class TileEntityLoadedBase extends TileEntity implements ILoadedTile {
         super.onLoad();
         this.isLoaded = true;
     }
+
+	public AudioWrapperV2 createAudioLoopV2() { return null; } //Vidarin: Remember to override this if you use rebootAudio!!
+
+	public AudioWrapperV2 rebootAudio(AudioWrapperV2 wrapper) {
+		wrapper.stopSound();
+		AudioWrapperV2 audio = createAudioLoopV2();
+		audio.startSound();
+		return audio;
+	}
 }
