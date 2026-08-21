f6bad4670a4ddb8f3dda4aea8caa1634508b439a
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:01:12 +0800

    Update TileEntityNukeCustom.java
    Fixed an issue where Nuke Custom did not calculate the explosion level in detail from the number of explosives in a single inventory slot.

--- src/main/java/com/hbm/tileentity/bomb/TileEntityNukeCustom.java
+++ src/main/java/com/hbm/tileentity/bomb/TileEntityNukeCustom.java
@@ -261,26 +261,26 @@ public class TileEntityNukeCustom extends TileEntity implements ITickable {
 			if(ent.entry == EnumEntryType.ADD) {
 
 				switch(ent.type) {
-				case TNT: tnt += ent.value; break;
-				case NUKE: nuke += ent.value; break;
-				case HYDRO: hydro += ent.value; break;
-				case BALE: bale += ent.value; break;
-				case DIRTY: dirty += ent.value; break;
-				case SCHRAB: schrab += ent.value; break;
-				case SOL: sol += ent.value; break;
-				case EUPH: euph += ent.value; break;
+				case TNT: tnt += ent.value * stack.getCount(); break;
+				case NUKE: nuke += ent.value * stack.getCount(); break;
+				case HYDRO: hydro += ent.value * stack.getCount(); break;
+				case BALE: bale += ent.value * stack.getCount(); break;
+				case DIRTY: dirty += ent.value * stack.getCount(); break;
+				case SCHRAB: schrab += ent.value * stack.getCount(); break;
+				case SOL: sol += ent.value * stack.getCount(); break;
+				case EUPH: euph += ent.value * stack.getCount(); break;
 				}
 
 			} else if(ent.entry == EnumEntryType.MULT) {
 
 				switch(ent.type) {
-				case TNT: tntMod *= ent.value; break;
-				case NUKE: nukeMod *= ent.value; break;
-				case HYDRO: hydroMod *= ent.value; break;
-				case BALE: baleMod *= ent.value; break;
-				case DIRTY: dirtyMod *= ent.value; break;
-				case SOL: solMod *= ent.value; break;
-				case SCHRAB: schrabMod *= ent.value; break;
+				case TNT: tntMod *= ent.value * stack.getCount(); break;
+				case NUKE: nukeMod *= ent.value * stack.getCount(); break;
+				case HYDRO: hydroMod *= ent.value * stack.getCount(); break;
+				case BALE: baleMod *= ent.value * stack.getCount(); break;
+				case DIRTY: dirtyMod *= ent.value * stack.getCount(); break;
+				case SOL: solMod *= ent.value * stack.getCount(); break;
+				case SCHRAB: schrabMod *= ent.value * stack.getCount(); break;
 				}
 			}
 		}
