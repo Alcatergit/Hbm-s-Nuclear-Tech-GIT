818994ce7ff409761bef2ffdfa54fcd6b10a3422
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:04:29 +0800

    Update FFPipeNetworkMk2.java

--- src/main/java/com/hbm/forgefluid/FFPipeNetworkMk2.java
+++ src/main/java/com/hbm/forgefluid/FFPipeNetworkMk2.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import com.hbm.interfaces.IFluidPipeMk2;
 import net.minecraft.tileentity.TileEntity;
@@ -19,8 +18,6 @@ import net.minecraftforge.fluids.capability.IFluidTankProperties;
 
 public class FFPipeNetworkMk2 implements IFluidHandler {
 
-	protected static Random rand = new Random();
-	
 	protected Fluid type;
 	protected Map<BlockPos, TileEntity> fillables = new HashMap<BlockPos, TileEntity>();
 	protected Map<BlockPos, IFluidPipeMk2> pipes = new HashMap<BlockPos, IFluidPipeMk2>();
@@ -39,6 +36,8 @@ public class FFPipeNetworkMk2 implements IFluidHandler {
 		if(resource == null || resource.getFluid() != type)
 			return 0;
 		List<IFluidHandler> handlers = new ArrayList<IFluidHandler>();
+		List<Integer> capacities = new ArrayList<Integer>();
+		int totalCapacity = 0;
 		
 		Iterator<TileEntity> itr = fillables.values().iterator();
 		while(itr.hasNext()){
@@ -49,8 +48,13 @@ public class FFPipeNetworkMk2 implements IFluidHandler {
 			}
 			if(FFUtils.safeCheckCapa(te, CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)){
 				IFluidHandler h = te.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null);
-				if(h != null && h.fill(new FluidStack(resource.getFluid(), 1), false) > 0){
-					handlers.add(h);
+				if(h != null){
+					int cap = h.fill(new FluidStack(resource.getFluid(), Integer.MAX_VALUE), false);
+					if(cap > 0){
+						handlers.add(h);
+						capacities.add(cap);
+						totalCapacity += cap;
+					}
 				}
 			}
 		}
@@ -58,34 +62,100 @@ public class FFPipeNetworkMk2 implements IFluidHandler {
 		if(handlers.isEmpty())
 			return 0;
 		
-		int part = resource.amount/handlers.size();
-		int totalDrained = 0;
+		int totalFilled = 0;
 		int remaining = resource.amount;
-		//Drillgon200: Extra hacky compensation
-		int intRoundingCompensation = resource.amount-part*handlers.size();
-		rand.setSeed(((TileEntity)this.fillables.values().iterator().next()).getWorld().getTotalWorldTime());
-		int randomFillIndex = rand.nextInt(handlers.size());
-		for(int i = 0; i < handlers.size(); i++){
+		int remainingCapacity = totalCapacity;
+		
+		for(int i = 0; i < handlers.size() && remaining > 0; i++){
 			IFluidHandler consumer = handlers.get(i);
-			int vol = consumer.fill(new FluidStack(resource.getFluid(), randomFillIndex == i ? part + intRoundingCompensation : part), doFill);
-			totalDrained += vol;
+			int share;
+			if(i == handlers.size() - 1){
+				share = Math.min(remaining, capacities.get(i));
+			} else {
+				share = weightedShare(remaining, capacities.get(i), remainingCapacity, Math.min(remaining, capacities.get(i)));
+			}
+			if(share <= 0) {
+				remainingCapacity -= capacities.get(i);
+				continue;
+			}
+			int vol = consumer.fill(new FluidStack(resource.getFluid(), share), doFill);
+			totalFilled += vol;
 			remaining -= vol;
-			if(remaining <= 0)
-				return totalDrained;
+			remainingCapacity -= capacities.get(i);
 		}
 		
-		return totalDrained;
+		return totalFilled;
 	}
 
 	@Override
 	public FluidStack drain(FluidStack resource, boolean doDrain) {
-		//I'm not sure how I'm supposed to implement a drain for a fluid pipe network as it no longer has an internal tank.
-		return null;
+		if(resource == null || resource.getFluid() != type)
+			return null;
+		List<IFluidHandler> providers = new ArrayList<IFluidHandler>();
+		List<Integer> availabilities = new ArrayList<Integer>();
+		int totalAvail = 0;
+		
+		Iterator<TileEntity> itr = fillables.values().iterator();
+		while(itr.hasNext()){
+			TileEntity te = itr.next();
+			if(te.isInvalid()){
+				itr.remove();
+				continue;
+			}
+			if(FFUtils.safeCheckCapa(te, CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)){
+				IFluidHandler h = te.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null);
+				if(h != null){
+					FluidStack test = h.drain(new FluidStack(type, Integer.MAX_VALUE), false);
+					if(test != null && test.amount > 0){
+						providers.add(h);
+						availabilities.add(test.amount);
+						totalAvail += test.amount;
+					}
+				}
+			}
+		}
+		
+		if(providers.isEmpty())
+			return null;
+		
+		int totalDrained = 0;
+		int remaining = resource.amount;
+		int remainingAvail = totalAvail;
+		
+		for(int i = 0; i < providers.size() && remaining > 0; i++){
+			IFluidHandler provider = providers.get(i);
+			int share;
+			if(i == providers.size() - 1){
+				share = Math.min(remaining, availabilities.get(i));
+			} else {
+				share = weightedShare(remaining, availabilities.get(i), remainingAvail, Math.min(remaining, availabilities.get(i)));
+			}
+			if(share <= 0) {
+				remainingAvail -= availabilities.get(i);
+				continue;
+			}
+			FluidStack drained = provider.drain(new FluidStack(type, share), doDrain);
+			if(drained != null){
+				totalDrained += drained.amount;
+				remaining -= drained.amount;
+			}
+			remainingAvail -= availabilities.get(i);
+		}
+		
+		return totalDrained > 0 ? new FluidStack(type, totalDrained) : null;
 	}
 
 	@Override
 	public FluidStack drain(int maxDrain, boolean doDrain) {
 		return null;
+	}
+
+	private static int weightedShare(int total, int part, int whole, int cap) {
+		if(total <= 0 || part <= 0 || whole <= 0 || cap <= 0) return 0;
+		if(part >= whole) return Math.min(total, cap);
+		int share = (int)(((long)total * part) / whole);
+		if(share <= 0) return 0;
+		return Math.min(share, cap);
 	}
 
 	public int size() {
@@ -94,6 +164,14 @@ public class FFPipeNetworkMk2 implements IFluidHandler {
 	
 	public Fluid getType() {
 		return type;
+	}
+
+	public Map<BlockPos, IFluidPipeMk2> getPipePositions() {
+		return pipes;
+	}
+
+	public Map<BlockPos, TileEntity> getFillableTiles() {
+		return fillables;
 	}
 
 	public void destroy() {
