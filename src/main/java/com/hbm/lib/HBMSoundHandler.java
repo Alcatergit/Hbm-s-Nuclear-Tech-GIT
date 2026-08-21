39f5e27eb0d872dd04d206efb77f811aae97f1b7
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:00:57 +0800

    Update HBMSoundHandler.java
    Add custom sound effects for Rod of Discord.

--- src/main/java/com/hbm/lib/HBMSoundHandler.java
+++ src/main/java/com/hbm/lib/HBMSoundHandler.java
@@ -203,6 +203,8 @@ public final class HBMSoundHandler {
 	public static SoundEvent hit_metal;
 	public static SoundEvent hit_flesh;
 	public static SoundEvent vomit;
+	// Register custom sound effects for Rod of Discord
+	public static SoundEvent discordTeleport;
 	public static SoundEvent chekhov_fire;
 	public static SoundEvent jeremy_fire;
 	public static SoundEvent jeremy_reload;
@@ -463,6 +465,8 @@ public final class HBMSoundHandler {
 		hit_metal = register("weapon.hit_metal");
 		hit_flesh = register("weapon.hit_flesh");
 		vomit = register("entity.vomit");
+		// Register custom sound effects for Rod of Discord
+		discordTeleport = register("item.discord_teleport");
 		chekhov_fire = register("turret.chekhov_fire");
 		jeremy_fire = register("turret.jeremy_fire");
 		jeremy_reload = register("turret.jeremy_reload");
