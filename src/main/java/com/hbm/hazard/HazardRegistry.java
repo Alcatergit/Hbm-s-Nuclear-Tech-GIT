ec3fa3972e3739b9611e322ab94a15c7f8494865
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:02:17 +0800

    Update HazardRegistry.java
    Add the self-ignition and radiation attributes to pribris_radiating and pribris_burning items.

--- src/main/java/com/hbm/hazard/HazardRegistry.java
+++ src/main/java/com/hbm/hazard/HazardRegistry.java
@@ -526,6 +526,8 @@ public class HazardRegistry {
         HazardSystem.register(digamma_matter, makeData(DIGAMMA, 0.2F));
         HazardSystem.register(particle_digamma, makeData(RADIATION, 100F).addEntry(DIGAMMA, 0.3333F));
 		HazardSystem.register(pribris_digamma, makeData(DIGAMMA, 133.33F));
+		HazardSystem.register(pribris_burning, makeData(HOT, 0.45F));
+		HazardSystem.register(pribris_radiating, makeData(RADIATION, 33.33F).addEntry(HOT, 1.33F));
 		
 		HazardSystem.register(frozen_grass, makeData(CRYOGENIC, 3));
 		HazardSystem.register(frozen_log, makeData(CRYOGENIC, 2));
