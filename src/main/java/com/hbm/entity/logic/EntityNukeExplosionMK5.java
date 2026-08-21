13ad8181ef98b7117fe4a13b4ee82c58d20e416c
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:02:08 +0800

    Update EntityNukeExplosionMK5.java
    Some parameters have been corrected.

--- src/main/java/com/hbm/entity/logic/EntityNukeExplosionMK5.java
+++ src/main/java/com/hbm/entity/logic/EntityNukeExplosionMK5.java
@@ -90,7 +90,7 @@ public class EntityNukeExplosionMK5 extends EntityChunky {
 
 		// Community-based radiation damage concept: Radiation is only applied in the initial stages of the explosion, using ray tracing calculations.
 		List<Entity> list = getEntitiesInRadius(world, this.posX, this.posY, this.posZ, this.radius * 2.0D);
-		if (fallout && explosion != null && this.ticksExisted < Math.max(1, radius / 10) && strength >= 75) {
+		if (fallout && explosion != null && this.ticksExisted < Math.max(1, radius * 0.1) && strength >= 75) {
 			List<EntityLivingBase> livingList = new ArrayList<>(list.size());
 			for (Entity e : list) if (e instanceof EntityLivingBase livingBase) livingList.add(livingBase);
 			radiate(livingList, (2_500_000F * radius * 0.05F) / (this.ticksExisted * 5 + 1));
@@ -208,7 +208,7 @@ public class EntityNukeExplosionMK5 extends EntityChunky {
             if(res < 1)
                 res = 1;
 
-            if(this.ticksExisted < (int)(Math.max(1, radius / 2)) && res < 2) {
+            if(this.ticksExisted < (int)(Math.max(1, radius * 0.5)) && res < 2) {
                 float fireDamage = (float)((0.5F * Math.pow(radius + 10, 3) * Math.pow(0.5, 0.5 * this.ticksExisted / radius)) / (dmgLen * dmgLen * dmgLen));
                 if(fireDamage > 0.025){
                     if (fireDamage > 0.1 && e instanceof EntityPlayer p) {
