959cde7326762459660feab283ef2d6313da7e10
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:07:43 +0800

    Update RenderTorex.java

--- src/main/java/com/hbm/render/entity/effect/RenderTorex.java
+++ src/main/java/com/hbm/render/entity/effect/RenderTorex.java
@@ -3,14 +3,12 @@ package com.hbm.render.entity.effect;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Random;
-import java.util.List;
 
 import com.hbm.main.ModEventHandlerClient;
 import org.lwjgl.opengl.GL11;
 
 import com.hbm.entity.effect.EntityNukeTorex;
 import com.hbm.entity.effect.EntityNukeTorex.Cloudlet;
-import com.hbm.explosion.ExplosionLarge;
 import com.hbm.lib.RefStrings;
 import com.hbm.main.MainRegistry;
 import com.hbm.render.amlfrom1710.Vec3;
@@ -29,22 +27,6 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
-import org.lwjgl.opengl.GL13;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
-
-import com.hbm.handler.HbmShaderManager2.Shader;
-import com.hbm.render.GLCompat;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.shader.Framebuffer;
-import net.minecraft.entity.Entity;
-
-import java.io.InputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import org.apache.commons.io.IOUtils;
-import org.lwjgl.BufferUtils;
 
 public class RenderTorex extends Render<EntityNukeTorex> {
 
@@ -56,10 +38,6 @@ public class RenderTorex extends Render<EntityNukeTorex> {
 	public static final int flashBaseDuration = 30;
 	public static final int flareBaseDuration = 100;
 
-	private static final List<Shockwave> warpActive = new ArrayList<>();
-	private static Framebuffer warpSceneCopy;
-	private static Shader warpShader;
-
 	protected RenderTorex(RenderManager renderManager){
 		super(renderManager);
 	}
@@ -69,7 +47,6 @@ public class RenderTorex extends Render<EntityNukeTorex> {
 		float scale = (float)cloud.getScale();
 		float flashDuration = scale * flashBaseDuration;
 		float flareDuration = scale * flareBaseDuration;
-		spawnWarp(cloud);
 
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
@@ -309,268 +286,5 @@ public class RenderTorex extends Render<EntityNukeTorex> {
 	@Override
 	protected ResourceLocation getEntityTexture(EntityNukeTorex entity) {
 		return null;
-	}
-
-	public static void spawnWarp(EntityNukeTorex entity) {
-		if ((entity == null || entity.isDead) || (entity.isReloaded && !entity.isScaled)) return;
-		for (Shockwave sw : warpActive) {
-			if (sw.entity == entity) return;
-		}
-
-		double s = entity.getScale();
-		int localTicks = Math.max((int) (entity.ticksExisted - (s * 2.5)), 0);
-
-		int explosionRadius = (int) (s * 100.0);
-		int blastDuration = (int) Math.ceil(80 * Math.cbrt(explosionRadius / 100.0));
-		double shockSpeed = Math.max(2.0, 2.0 * explosionRadius / (double) blastDuration);
-		float maxRadius = (float) (2.0 * explosionRadius);
-		float lifetime = (float) (maxRadius / shockSpeed);
-		if (localTicks < 1) return;
-
-		float spawnTick = 0;
-		if (localTicks == 1) {
-			spawnTick = entity.ticksExisted;
-		} else if (entity.isReloaded && entity.isScaled) {
-			spawnTick = (int) Math.ceil(s * 2.5 + 1);
-			if (entity.ticksExisted - spawnTick >= lifetime) return;
-		}
-
-		warpActive.add(new Shockwave(entity.posX, entity.posY, entity.posZ, maxRadius, lifetime, (float) s, entity, spawnTick));
-	}
-
-	public static void spawnWarpForExplosion(ExplosionLarge.EntityShockwave entity) {
-		if (entity == null || entity.isDead || entity.explosionRadius <= 0) return;
-		for (Shockwave sw : warpActive) {
-			if (sw.entity == entity) return;
-		}
-
-		int explosionRadius = entity.explosionRadius;
-		int blastDuration = (int) Math.ceil(80 * Math.cbrt(explosionRadius / 100.0));
-		double shockSpeed = Math.max(2.0, 2.0 * explosionRadius / (double) blastDuration);
-		float maxRadius = (float) (2.0 * explosionRadius);
-		float lifetime = (float) (maxRadius / shockSpeed);
-
-		if (entity.ticksExisted >= lifetime) return;
-
-		float scale = explosionRadius / 100.0F;
-		warpActive.add(new Shockwave(entity.posX, entity.posY, entity.posZ, maxRadius, lifetime, scale, entity, 0));
-	}
-
-	public static void renderWarp(float partialTicks) {
-		if (warpActive.isEmpty()) return;
-
-		Minecraft mc = Minecraft.getMinecraft();
-		if (mc.world == null || mc.getRenderViewEntity() == null) return;
-
-		warpActive.removeIf(sw -> sw.entity == null || sw.entity.world != mc.world || sw.entity.isDead || sw.getAge(partialTicks) >= sw.lifetime);
-		if (warpActive.isEmpty()) return;
-
-		if (warpShader == null) {
-			warpShader = loadWarpShader();
-		}
-		int shader = warpShader.getShaderId();
-		if (shader == 0) {
-			return;
-		}
-
-		Framebuffer mainFbo = mc.getFramebuffer();
-		int w = mainFbo.framebufferWidth;
-		int h = mainFbo.framebufferHeight;
-
-		ensureWarpSceneCopy(w, h);
-
-		GL30.glBindFramebuffer(GL30.GL_READ_FRAMEBUFFER, mainFbo.framebufferObject);
-		GL30.glBindFramebuffer(GL30.GL_DRAW_FRAMEBUFFER, warpSceneCopy.framebufferObject);
-		GL30.glBlitFramebuffer(0, 0, w, h, 0, 0, w, h, GL11.GL_COLOR_BUFFER_BIT, GL11.GL_NEAREST);
-		GL30.glBindFramebuffer(GL30.GL_DRAW_FRAMEBUFFER, mainFbo.framebufferObject);
-
-		GlStateManager.pushMatrix();
-		GlStateManager.pushAttrib();
-
-		GlStateManager.depthMask(false);
-		GlStateManager.enableDepth();
-		GlStateManager.enableBlend();
-		GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
-		GlStateManager.disableCull();
-		GlStateManager.disableLighting();
-		GlStateManager.enableTexture2D();
-
-		GL13.glActiveTexture(GL13.GL_TEXTURE0);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, warpSceneCopy.framebufferTexture);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_CLAMP);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_CLAMP);
-
-		OpenGlHelper.glUseProgram(shader);
-		GL20.glUniform2f(GL20.glGetUniformLocation(shader, "screenSize"), w, h);
-
-		Entity viewer = mc.getRenderViewEntity();
-		double viewX = viewer.lastTickPosX + (viewer.posX - viewer.lastTickPosX) * partialTicks;
-		double viewY = viewer.lastTickPosY + (viewer.posY - viewer.lastTickPosY) * partialTicks;
-		double viewZ = viewer.lastTickPosZ + (viewer.posZ - viewer.lastTickPosZ) * partialTicks;
-
-		for (Shockwave sw : warpActive) {
-			float age = sw.getAge(partialTicks);
-			float radius = sw.getRadius(age);
-			float alpha = sw.getAlpha(age);
-			if (alpha <= 0.01F) continue;
-
-			GL20.glUniform1f(GL20.glGetUniformLocation(shader, "intensity"), alpha * sw.intensityScale);
-			GL20.glUniform1f(GL20.glGetUniformLocation(shader, "time"), sw.entity.ticksExisted + partialTicks);
-
-			GlStateManager.pushMatrix();
-			GlStateManager.translate(sw.x - viewX, sw.y - viewY, sw.z - viewZ);
-			GlStateManager.scale(radius, radius, radius);
-
-			renderWarpSphere(16, 48);
-
-			GlStateManager.popMatrix();
-		}
-
-		OpenGlHelper.glUseProgram(0);
-
-		GlStateManager.setActiveTexture(GL13.GL_TEXTURE0);
-		GlStateManager.bindTexture(0);
-
-		GlStateManager.popAttrib();
-		GlStateManager.popMatrix();
-
-		GlStateManager.enableTexture2D();
-		GlStateManager.enableDepth();
-		GlStateManager.depthMask(true);
-		GlStateManager.disableBlend();
-		GlStateManager.enableCull();
-		GlStateManager.enableLighting();
-		GlStateManager.enableAlpha();
-		GlStateManager.colorMask(true, true, true, true);
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-
-		mc.getFramebuffer().bindFramebuffer(true);
-	}
-
-	private static void ensureWarpSceneCopy(int width, int height) {
-		if (warpSceneCopy == null || warpSceneCopy.framebufferWidth != width
-				|| warpSceneCopy.framebufferHeight != height) {
-			if (warpSceneCopy != null) warpSceneCopy.deleteFramebuffer();
-			warpSceneCopy = new Framebuffer(width, height, false);
-			warpSceneCopy.setFramebufferColor(0, 0, 0, 0);
-		}
-	}
-
-	private static void renderWarpSphere(int stacks, int slices) {
-		for (int i = 0; i < stacks; i++) {
-			float lat0 = (float) (Math.PI * (-0.5 + (double) i / stacks));
-			float z0 = (float) Math.sin(lat0);
-			float zr0 = (float) Math.cos(lat0);
-
-			float lat1 = (float) (Math.PI * (-0.5 + (double) (i + 1) / stacks));
-			float z1 = (float) Math.sin(lat1);
-			float zr1 = (float) Math.cos(lat1);
-
-			GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
-			for (int j = 0; j <= slices; j++) {
-				float lng = (float) (2 * Math.PI * (double) j / slices);
-				float x = (float) Math.cos(lng);
-				float y = (float) Math.sin(lng);
-
-				float nx0 = x * zr0;
-				float ny0 = y * zr0;
-				float nz0 = z0;
-				GL11.glNormal3f(nx0, ny0, nz0);
-				GL11.glVertex3f(nx0, ny0, nz0);
-
-				float nx1 = x * zr1;
-				float ny1 = y * zr1;
-				float nz1 = z1;
-				GL11.glNormal3f(nx1, ny1, nz1);
-				GL11.glVertex3f(nx1, ny1, nz1);
-			}
-			GL11.glEnd();
-		}
-	}
-
-	private static Shader loadWarpShader() {
-		try {
-			ResourceLocation file = new ResourceLocation(RefStrings.MODID, "shaders/warp_shockwave");
-			int program = GLCompat.createProgram();
-
-			int vertexShader = GLCompat.createShader(GLCompat.GL_VERTEX_SHADER);
-			GLCompat.shaderSource(vertexShader, readShaderFile(new ResourceLocation(file.getNamespace(), file.getPath() + ".vert")));
-			GLCompat.compileShader(vertexShader);
-			if (GLCompat.getShaderi(vertexShader, GLCompat.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
-				MainRegistry.logger.error(GLCompat.getShaderInfoLog(vertexShader, GLCompat.GL_INFO_LOG_LENGTH));
-				throw new RuntimeException("Error creating warp vertex shader");
-			}
-
-			int fragmentShader = GLCompat.createShader(GLCompat.GL_FRAGMENT_SHADER);
-			GLCompat.shaderSource(fragmentShader, readShaderFile(new ResourceLocation(file.getNamespace(), file.getPath() + ".frag")));
-			GLCompat.compileShader(fragmentShader);
-			if (GLCompat.getShaderi(fragmentShader, GLCompat.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
-				MainRegistry.logger.error(GLCompat.getShaderInfoLog(fragmentShader, GLCompat.GL_INFO_LOG_LENGTH));
-				throw new RuntimeException("Error creating warp fragment shader");
-			}
-
-			GLCompat.attachShader(program, vertexShader);
-			GLCompat.attachShader(program, fragmentShader);
-			GLCompat.linkProgram(program);
-			if (GLCompat.getProgrami(program, GLCompat.GL_LINK_STATUS) == GL11.GL_FALSE) {
-				MainRegistry.logger.error(GLCompat.getProgramInfoLog(program, GLCompat.GL_INFO_LOG_LENGTH));
-				throw new RuntimeException("Error linking warp shader");
-			}
-
-			GLCompat.deleteShader(vertexShader);
-			GLCompat.deleteShader(fragmentShader);
-
-			return new Shader(program).withUniforms(s -> s.uniform1i("sceneTex", 0));
-		} catch (Exception x) {
-			MainRegistry.logger.error("Failed to load warp shader, warp effect disabled");
-			x.printStackTrace();
-			return new Shader(0);
-		}
-	}
-
-	private static ByteBuffer readShaderFile(ResourceLocation file) throws IOException {
-		InputStream in = Minecraft.getMinecraft().getResourceManager().getResource(file).getInputStream();
-		byte[] bytes = IOUtils.toByteArray(in);
-		IOUtils.closeQuietly(in);
-		ByteBuffer buf = BufferUtils.createByteBuffer(bytes.length);
-		buf.put(bytes);
-		buf.rewind();
-		return buf;
-	}
-
-	private static class Shockwave {
-		final double x, y, z;
-		final float maxRadius, lifetime, intensityScale;
-		final Entity entity;
-		final float spawnTick;
-
-		Shockwave(double x, double y, double z, float maxRadius, float lifetime, float scale, Entity entity, float spawnTick) {
-			this.x = x;
-			this.y = y;
-			this.z = z;
-			this.maxRadius = maxRadius;
-			this.lifetime = lifetime;
-			this.intensityScale = 1F;
-			this.entity = entity;
-			this.spawnTick = spawnTick;
-		}
-
-		float getAge(float partialTicks) {
-			return entity.ticksExisted + partialTicks - spawnTick;
-		}
-
-		float getRadius(float age) {
-			float t = Math.min(age / lifetime, 1.0F);
-			return maxRadius * t + 1.0F;
-		}
-
-		float getAlpha(float age) {
-			float fadeStart = lifetime * 0.72F;
-			if (age < fadeStart) return 1.0F;
-			float fadeT = (age - fadeStart) / (lifetime - fadeStart);
-			return (float) (1.0 - Math.pow(fadeT, 3.0));
-		}
 	}
 }
