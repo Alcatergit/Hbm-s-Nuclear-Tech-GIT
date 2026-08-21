85b2547374cba486dc14c2167c04b9592cbcff02
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-21 15:06:22 +0800

    Update TileEntityRBMKHeater.java

--- src/main/java/com/hbm/tileentity/machine/rbmk/TileEntityRBMKHeater.java
+++ src/main/java/com/hbm/tileentity/machine/rbmk/TileEntityRBMKHeater.java
@@ -125,7 +125,7 @@ public class TileEntityRBMKHeater extends TileEntityRBMKSlottedBase implements I
 
 	public void fillFluidInit(FluidTank tank) {
 
-		fillFluid(this.pos.getX(), this.pos.getY() + rbmkHeight + 1, this.pos.getZ(), tank);
+		fillFluid(this.pos.getX(), this.pos.getY() + RBMKDials.getColumnHeight(world) + 1, this.pos.getZ(), tank);
 		
 		if(world.getBlockState(new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ())).getBlock() == ModBlocks.rbmk_loader) {
 
