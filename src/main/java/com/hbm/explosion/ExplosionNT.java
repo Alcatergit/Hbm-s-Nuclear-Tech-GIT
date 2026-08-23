31d5290c0ddf186110a6aef63dec4ad8c30c029e
Rebuild-of-Evangelion <62316417+Rebuild-of-Evangelion@users.noreply.github.com>
2026-08-23 20:31:15 +0800

    Update ExplosionNT.java

--- src/main/java/com/hbm/explosion/ExplosionNT.java
+++ src/main/java/com/hbm/explosion/ExplosionNT.java
@@ -248,6 +248,7 @@ public class ExplosionNT extends Explosion {
 				if(block.isNormalCube()) {
 					
 					if(has(ExAttrib.DIGAMMA)) {
+						this.worldObj.removeTileEntity(chunkposition);
 						this.worldObj.setBlockState(new BlockPos(i, j, k), ModBlocks.ash_digamma.getDefaultState());
 						
 						if(this.explosionRNG.nextInt(5) == 0 && this.worldObj.getBlockState(new BlockPos(i, j + 1, k)).getBlock() == Blocks.AIR)
@@ -256,16 +257,20 @@ public class ExplosionNT extends Explosion {
 					} else if(has(ExAttrib.DIGAMMA_CIRCUIT)) {
 						
 						if(i % 3 == 0 && k % 3 == 0) {
+							this.worldObj.removeTileEntity(chunkposition);
 							this.worldObj.setBlockState(new BlockPos(i, j, k), ModBlocks.pribris_digamma.getDefaultState());
 						} else if((i % 3 == 0 || k % 3 == 0) && this.explosionRNG.nextBoolean()) {
+							this.worldObj.removeTileEntity(chunkposition);
 							this.worldObj.setBlockState(new BlockPos(i, j, k), ModBlocks.pribris_digamma.getDefaultState());
 						} else {
+							this.worldObj.removeTileEntity(chunkposition);
 							this.worldObj.setBlockState(new BlockPos(i, j, k), ModBlocks.ash_digamma.getDefaultState());
 							
 							if(this.explosionRNG.nextInt(5) == 0 && this.worldObj.getBlockState(new BlockPos(i, j + 1, k)).getBlock() == Blocks.AIR)
 								this.worldObj.setBlockState(new BlockPos(i, j + 1, k), ModBlocks.fire_digamma.getDefaultState());
 						}
 					} else if(has(ExAttrib.LAVA_V)) {
+						this.worldObj.removeTileEntity(chunkposition);
 						this.worldObj.setBlockState(new BlockPos(i, j, k), ModBlocks.volcanic_lava_block.getDefaultState());
 					}
 				}
@@ -328,5 +333,4 @@ public class ExplosionNT extends Explosion {
 		NOSOUND,
 		NOHURT
 	}
-
 }
